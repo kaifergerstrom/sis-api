8d79fb8c5c6da47833e2d03a6adf77653b27cbbe
kaifergerstrom <kaifergerstrom@gmail.com>
2019-10-14 14:33:08 -0400

    Updated login logic for new sign in form

--- main.py
+++ main.py
@@ -4,90 +4,31 @@ import collections
 import json
 import argparse
 
-# Argparser for username input
-parser = argparse.ArgumentParser(description='Input SIS Information')
-parser.add_argument('--username', dest='username', type=str, help='SIS Student Username')
-parser.add_argument('--password', dest='password', type=str, help='SIS Student Password')
-args = parser.parse_args()
 
-def get_grade_links(soup):  # Ugly function to compile all the links to other quarters into an dictionary
-	quarters = {}
-	for link in links.find_all('a'):
-		quarters[link.text] = link['href']
-	selected = links.find("li", {"class": "selected"})
-	quarters[selected.text] = gradebook_page.split("/")[-1]
-	return quarters
+class SISpy:
+	# Links for pages in SIS
+	p_login = "https://sisstudent.fcps.edu/SVUE/"
+	p_gradebook = "https://sisstudent.fcps.edu/SVUE/PXP2_Gradebook.aspx?AGU=0"
 
+	gradebook = {}
 
-# Links for pages in SIS
-login_page = "https://sisstudent.fcps.edu/SVUE/"
-gradebook_page = "https://sisstudent.fcps.edu/SVUE/PXP_Gradebook.aspx?AGU=0"
+	def __init__(self):
+		with requests.Session() as s:  # Create a requests session
+			page = s.get(self.p_login)  # Navigate to the login page
+			s_login = BeautifulSoup(page.content, features="html.parser")  # Create a parser for the gradebook
 
-with requests.Session() as s:  # Create a requests session
+			# Prepare the data to post to the login form (apsx)
+			data = {}
+			data['ctl00$MainContent$username'] = ""
+			data['ctl00$MainContent$password'] = ""
+			data["__VIEWSTATE"] = s_login.select_one("#__VIEWSTATE")["value"]
+			data["__VIEWSTATEGENERATOR"] = s_login.select_one("#__VIEWSTATEGENERATOR")["value"]
+			data["__EVENTVALIDATION"] = s_login.select_one('#__EVENTVALIDATION')['value']
 
-	page = s.get(login_page)  # Navigate to the login page
-	login_soup = BeautifulSoup(page.content, features="html.parser")  # Create a parser for the gradebook
+			# Post data to form, open the gradebook with session
+			s.post(self.p_login, data=data)
+			r_gradebook = s.get(self.p_gradebook)
 
-	# Prepare the data to post to the login form (apsx)
-	data = {}
-	data['username'] = args.username
-	data['password'] = args.password
-	data["__VIEWSTATE"] = login_soup.select_one("#__VIEWSTATE")["value"]
-	data["__VIEWSTATEGENERATOR"] = login_soup.select_one("#__VIEWSTATEGENERATOR")["value"]
-	data["__EVENTVALIDATION"] = login_soup.select_one('#__EVENTVALIDATION')['value']
+			s_grades = BeautifulSoup(r_gradebook.content, "html.parser")  # Create a parser for the gradebook
 
-	# Post data to form, open the gradebook with session
-	s.post(login_page, data=data)
-	gradebook = s.get(gradebook_page)
-
-	soup = BeautifulSoup(gradebook.content, "html.parser")  # Create a parser for the gradebook
-
-	links = soup.find("div", {"class": "heading_breadcrumb"})
-	links = get_grade_links(links)
-	
-	courses = collections.defaultdict(dict)  # Parent dictionary for course data
-
-	for key, link in links.items():
-
-		url = "https://sisstudent.fcps.edu/SVUE/{}".format(link)
-		page = s.get(url)  # Navigate to the login page
-		link_soup = BeautifulSoup(page.content, "html.parser")
-
-		# Find the table storing grade data and skip first row (header)
-		table = link_soup.find("table", {"class": "info_tbl"})
-		rows = iter(table.find_all('tr'))
-		next(rows)
-		
-		for row in rows:  # Loop through all the rows
-
-			cells = row.findAll("td")
-
-			# Iterate over cells in table and construct dictionary format
-			course = {}
-			course["period"] = cells[0].text.strip()
-			course["title"] = cells[1].text.strip()
-			course["teacher"] = cells[4].text.strip()
-			course["grades"] = cells[5].text.strip()
-			course["final_mark"] = cells[7].text.strip()
-
-			courses[key][course["period"]] = course
-
-	def get_grades(period):
-		grades = {}
-		for quarter, classes in courses.items():
-			grades[quarter] = classes[str(period)]['grades']
-		return grades
-
-	formatted = {}
-	for quarter, classes in courses.items():
-		for period, data in classes.items():
-			formatted[period] = data
-
-	for period, data in formatted.items():
-		formatted[period]['grades'] = get_grades(period)
-
-	with open('courses.json', 'w+') as outfile:  # Save formatted json file
-		json.dump(formatted, outfile, indent=4)
-		print('Finished and saved to "courses.json"')
-
-
+SISpy = SISpy()
